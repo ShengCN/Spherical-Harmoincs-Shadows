ebc2017916b4ff5af950ca8ca686f573ce17638f
ShengCN <451718145@qq.com>
2020-10-13 03:27:56 -0400

    add glfw

---
+++ testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/DependInfo.cmake
@@ -0,0 +1,76 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/glad/glad.c" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/glad/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "BOOST_ALL_NO_LIB"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "GLM_FORCE_RADIANS"
+  "IMGUI_IMPL_OPENGL_LOADER_GLAD=1"
+  "graphics_lib_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../graphics_lib/Dep/stb"
+  "../graphics_lib/Dep/imgui"
+  "../graphics_lib/Dep"
+  "../graphics_lib"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/imgui/examples/imgui_impl_glfw.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/imgui/examples/imgui_impl_glfw.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/imgui/examples/imgui_impl_opengl3.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/imgui/examples/imgui_impl_opengl3.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/imgui/imgui.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/imgui/imgui.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/imgui/imgui_demo.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/imgui/imgui_demo.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/imgui/imgui_draw.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/imgui/imgui_draw.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/imgui/imgui_widgets.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/imgui/imgui_widgets.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/tinyobjloader/tiny_obj_loader.cc" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/tinyobjloader/tiny_obj_loader.cc.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/tinyxml2/tinyxml2.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/tinyxml2/tinyxml2.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Dep/tinyxml2/xmltest.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Dep/tinyxml2/xmltest.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Render/material.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Render/material.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Render/mesh.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Render/mesh.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Render/ppc.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Render/ppc.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Render/scene.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Render/scene.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Render/shader.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Render/shader.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Utilities/Logger.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Utilities/Logger.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Utilities/Utils.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Utilities/Utils.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Utilities/model_loader.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Utilities/model_loader.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/Utilities/voxelization.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/Utilities/voxelization.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/asset_manager.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/asset_manager.cpp.o"
+  "/home/ysheng/Documents/CGT_620/graphics_lib/render_engine.cpp" "/home/ysheng/Documents/CGT_620/testbuild/graphics_lib/CMakeFiles/graphics_lib.dir/render_engine.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "GLM_FORCE_RADIANS"
+  "IMGUI_IMPL_OPENGL_LOADER_GLAD=1"
+  "graphics_lib_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../graphics_lib/Dep/stb"
+  "../graphics_lib/Dep/imgui"
+  "../graphics_lib/Dep"
+  "../graphics_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
